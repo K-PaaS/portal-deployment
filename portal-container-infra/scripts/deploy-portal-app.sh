254689c8713ea8bee6ece40d5852d65d76d78858
okpc579 <ski579579@naver.com>
2023-12-06 06:45:44 +0000

    k-paas logo

--- portal-container-infra/scripts/deploy-portal-app.sh
+++ portal-container-infra/scripts/deploy-portal-app.sh
@@ -2,19 +2,19 @@
 
 source portal-app-variable.yml
 
-PORTALAPPNAME=portal-app-1.2.14.1
-PORTALAPPDOWNLOADLINK=https://nextcloud.k-paas.org/index.php/s/yzND6SjwiFG8yfy/download
+PORTALAPPNAME=portal-app-1.2.15
+PORTALAPPDOWNLOADLINK=https://nextcloud.k-paas.org/index.php/s/XkWAFaE8d5S7Mqd/download
 
 #########################################
 # Portal Component Folder Name
-PORTAL_API=portal-api-2.4.3.1
-PORTAL_COMMON_API=portal-common-api-2.2.6.1
+PORTAL_API=portal-api-2.4.4
+PORTAL_COMMON_API=portal-common-api-2.2.7
 PORTAL_GATEWAY=portal-gateway-2.1.2.1
 PORTAL_LOG_API=portal-log-api-2.3.2.1
 PORTAL_REGISTRATION=portal-registration-2.1.0.1
-PORTAL_STORAGE_API=portal-storage-api-2.2.1.1
-PORTAL_WEB_ADMIN=portal-web-admin-2.3.5.1
-PORTAL_WEB_USER=portal-web-user-2.4.10.1
+PORTAL_STORAGE_API=portal-storage-api-2.2.2
+PORTAL_WEB_ADMIN=portal-web-admin-2.3.6
+PORTAL_WEB_USER=portal-web-user-2.4.11
 PORTAL_SSH=portal-ssh-1.0.0.1
 
 #########################################
